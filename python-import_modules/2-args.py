1f9665837b27ceb754d3306f29daebf8a2442358
hasanoffh <ilgarhasanof@gmail.com>
2026-04-11 11:06:41 +0400

    Fix pycodestyle whitespace issues in 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,13 +4,13 @@ import sys
 if __name__ == "__main__":
     argv = sys.argv[1:]
     count = len(argv)
-    
+
     if count == 0:
         print("0 arguments.")
     elif count == 1:
         print("1 argument:")
     else:
         print("{:d} arguments:".format(count))
-        
+
     for i in range(count):
         print("{:d}: {:s}".format(i + 1, argv[i]))
