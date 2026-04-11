57c4e32bdbae1ae548c1bc51bd8acfab5c58d65e
hasanoffh <ilgarhasanof@gmail.com>
2026-04-11 11:27:40 +0400

    Fix W293: remove whitespace in 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Hər iki tuple üçün ilk 2 elementi təmin edirik
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
-    
-    # Yalnız ilk iki elementi toplayıb yeni tuple qaytarırıq
     return (a[0] + b[0], a[1] + b[1])
