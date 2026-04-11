83375599c22e79ac090a525f0275686c13f3e213
hasanoffh <ilgarhasanof@gmail.com>
2026-04-11 11:05:47 +0400

    Print the number of and the list of its arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    count = len(argv)
+    
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(count))
+        
+    for i in range(count):
+        print("{:d}: {:s}".format(i + 1, argv[i]))
