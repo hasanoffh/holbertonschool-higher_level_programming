673cb194a4c882b3074f2e74bdd092f7365fe8ed
hasanoffh <ilgarhasanof@gmail.com>
2026-04-17 07:39:24 +0000

    Fix E501 line too long in 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-A script that fetches https://intranet.hbtn.io/status using the requests package.
+A script that fetches https://intranet.hbtn.io/status
+using the requests package.
 """
 import requests
 
