4858c5cd009aa3006ea78df38ed12586ad3b194c
hasanoffh <ilgarhasanof@gmail.com>
2026-04-17 07:42:50 +0000

    Fix W293 blank line contains whitespace in 10-my_github.py

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -8,17 +8,9 @@ import sys
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    token = sys.argv[2]
-    url = "https://api.github.com/user"
-
-    # Basic Authentication istifadə edərək sorğu göndəririk
-    r = requests.get(url, auth=(username, token))
-    
+    auth = (sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
     try:
-        # Cavabı JSON formatında oxuyuruq
-        user_data = r.json()
-        # 'id' açarını çap edirik, yoxdursa None qaytarır
-        print(user_data.get('id'))
+        print(r.json().get("id"))
     except ValueError:
         print("None")
