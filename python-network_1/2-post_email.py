5c3532374b579157f5b786de7374d9dfc897cb7f
hasanoffh <ilgarhasanof@gmail.com>
2026-04-17 07:34:03 +0000

    Fix pycodestyle whitespace errors in 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -3,8 +3,8 @@
 A script that takes in a URL and an email, sends a POST request to the passed
 URL with the email as a parameter, and displays the body of the response.
 """
-import urllib.request
 import urllib.parse
+import urllib.request
 import sys
 
 
@@ -12,12 +12,8 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     values = {'email': email}
-    
-    # Məlumatı URL-encoded formatına salırıq
     data = urllib.parse.urlencode(values)
-    # Bayt formatına çeviririk (POST sorğusu üçün bu mütləqdir)
     data = data.encode('ascii')
-    
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
